0959f37354595806165c5d0d371323e654a4b0e7
unknown <Tomasz.Wojtyniak@aps55377-mb.poland.asseco.corp>
2017-01-08 23:15:48 +0100

    Module1 Coding Assignment

---
+++ module1-solutions/app.js
@@ -0,0 +1,39 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.message = "";
+  $scope.lunchMenu = "";
+
+
+  $scope.checkIfToMuch = function () {
+    var listOfItem = $scope.lunchMenu;
+    var arrayOfItem = listOfItem.split(",");
+
+    arrayOfItem = removeEmptyString(arrayOfItem);
+
+    if(arrayOfItem.length == 0){
+      $scope.message = "Please enter data first";
+    } else if (arrayOfItem.length <= 3) {
+      $scope.message = "Enjoy!";
+    } else {
+      $scope.message = "Too much!";
+    }
+  };
+
+  function removeEmptyString(arrayOfItem) {
+    var array = [];
+    for(var i=0;i<arrayOfItem.length;i++){
+      if(arrayOfItem[i].length != 0){
+          array.push(arrayOfItem[i]);
+      }
+    }
+    return array;
+  };
+}
+})();
